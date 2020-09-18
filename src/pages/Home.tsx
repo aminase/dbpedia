15ad13972e199a14d4e6136e30afad742b39b2ca
aminase <aminase991@gmail.com>
2020-09-18 12:15:14 +0200

    Progress on search input

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,37 +1,58 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import _ from 'lodash'
+import ReactJson from 'react-json-view'
 
 const API_ROOT = 'http://dbpedia.org/data/Matteo_Donati.json'
 
+const dataKey = 'http://dbpedia.org/resource/Matteo_Donati'
+// const heightKey = "http://dbpedia.org/ontology/Person/height";
+
+const searchKeyMapper = {
+  height: 'http://dbpedia.org/ontology/Person/height',
+  weight: 'sadf',
+}
+
 export const Home: React.FC = () => {
   const [data, setData] = useState({ hits: [] })
-  const [search, setSearch] = useState('')
+  const [searchTerm, setSearchTerm] = useState('')
+  const [error, setError] = useState('')
+  const [dataSubset, setDataSubset] = useState({})
+
   const [name, setName] = useState({
     name: [{ first: 'AMina', value: 2, surname: 'Edo' }],
   })
+
   console.log(name.name[0]['value'], 'name')
 
   useEffect(() => {
     const fetchData = async () => {
-      const response = await axios.get(`${API_ROOT}/Matteo_Donati.json`)
-      console.log(response, 'result 1')
+      const response = await axios.get(`${API_ROOT}`)
+      const data = _.get(response.data, dataKey)
 
-      const height = _.get(response, `${API_ROOT}/ontology/height`, '')
-      console.log(height, 'height')
+      setData(data)
     }
     fetchData()
   }, [])
 
-  const parsing = parseInt('4F', 16)
-  console.log(parsing, 'parse')
+  const searchItem = (e: any) => {
+    const searchTerm = e.target.value
+    setSearchTerm(searchTerm)
 
-  const searchItem = (e: any) => {}
+    const targetKeyPath = _.get(searchKeyMapper, searchTerm)
 
-  const firstObject = { name: 'Edo' }
-  firstObject.name = 'Amina'
+    const dataSubset = _.get(data, targetKeyPath)
+    setDataSubset(dataSubset)
 
-  console.log(firstObject, 'objc')
+    if (dataSubset) {
+      setError('')
+    } else {
+      setError('No matching dbpedia data found')
+    }
+
+    console.log('searchTerm---', searchTerm)
+    console.log('dataSubset---', dataSubset)
+  }
 
   return (
     <div className="m-10">
@@ -41,12 +62,10 @@ export const Home: React.FC = () => {
         type="text"
         onChange={(e) => searchItem(e)}
       />
+      {error !== '' && searchTerm !== '' && <>{error}</>}
 
-      {/* {data.hits.map((item) => (
-          <li key={item.objectID}>
-            <a href={item.url}>{item.title}</a>
-          </li>
-        ))} */}
+      <br />
+      {!_.isEmpty(dataSubset) && <ReactJson src={{ dataSubset }} />}
     </div>
   )
 }
